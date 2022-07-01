13810c3bfb42d454baa56fb4414b99740c2c8648
Max Carlquist <max.carlquist@zocom.se>
2022-07-01 10:25:16 +0200

    added files

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,37 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <button @click="getPosts">get Posts</button>
+    <div v-for="post in posts" :key="post.id">
+      <h3>{{ post.title }}</h3>
+      <small>{{ post.author }}</small>
+      <p>{{ post.body }}</p>
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from './components/HelloWorld.vue';
+import axios from 'axios';
 
 export default {
+  data() {
+    return {
+      posts: []
+    }
+  },
   name: 'App',
   components: {
     HelloWorld
-  }
+  },
+  methods: {
+    async getPosts() {
+      const res = await axios.get('http://localhost:1337/posts');
+      const data = await res.data;
+      this.posts = data;
+    }
+  },
 }
 </script>
 
